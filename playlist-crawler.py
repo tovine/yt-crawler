6ff753bb7b162e1cdc298b8285160ed78f4007c3
Torbjørn <tovine@users.noreply.github.com>
2014-11-17 15:32:50 +0100

    Added logging to file

--- playlist-crawler.py
+++ playlist-crawler.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-import os, sys, subprocess, getpass
+import os, sys, subprocess, getpass, time
 
 # Default settings
 path = '/home/'+getpass.getuser()+'/Electronica'
+logfile='/home/'+getpass.getuser()+'/ytcrawler.log'
 url = 'http://www.youtube.com/playlist?list=PLFPg_IUxqnZM3uua-YwStHJ1qmlQKBnh0'
 
 if (len(sys.argv) > 2):
@@ -15,6 +16,9 @@ elif (len(sys.argv) > 1):
 		exit("Simple script to keep a local folder in sync with a youtube playlist...\n\nUse: playlist-crawler.py [url] [path]\n	url:	can be video or playlist, default 'YouTube Top 100 Electronica playlist'\n	path:	Where to store downloaded videos, default: '"+path+"'\n\nBoth default variables are configurable, can be edited at the top of the file - enjoy!")
 	# Assuming first argument to be playlist-url
 	url = sys.argv[1]
+
+log = open(logfile,'a')
+log.write('Youtube-crawler started at '+time.strftime("%d-%m-%Y %H:%M:%S")+'\n----------------------------------------------\nPlaylist URL:\t'+url+'\nDestination folder:\t'+path+'\n\n')
 
 tmpfile = '/tmp/ytcrawl'
 command = "youtube-dl -ie --get-id "+url+" 1> "+tmpfile
@@ -29,6 +33,7 @@ def archive(filename):
 	cmd = 'cd '+path+' && mkdir -p archived && mv "'+filename+'" archived/'
 	if os.system(cmd) is not 0:
 		print('Failed to archive '+filename)
+		log.write('Error: failed to archive '+filename+'\n')
 		return False
 	return True
 
@@ -58,12 +63,18 @@ for existing in files:
 			archived.append(existing)
 			archive(existing)
 
-
 #print(vidlist)
 print("To be downloaded:")
 print(download)
 print("Files archived:")
 print(archived)
+
+log.write("To be downloaded:\n")
+for title in download:
+	log.write(title+'\n')
+log.write("Files archived:\n")
+for title in archived:
+	log.write(title+'\n')
 
 for url in download:
 	cmd = 'cd '+path+' && youtube-dl http://youtube.com/watch?v='+vidlist[url]
@@ -72,5 +83,5 @@ for url in download:
 
 #for file in files:
 #	print(file)
-
-
+log.write("Execution completed at "+time.strftime("%d-%m-%Y %H:%M:%S")+'\n\n')
+log.close()
